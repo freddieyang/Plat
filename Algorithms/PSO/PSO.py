74ffe1bbf8c08a0aa6ddc13984745970dd84d6ca
freddieyang <yf970113@outlook.com>
2021-04-07 17:23:47 +0800

    完成了PSO算法

--- Algorithms/PSO/PSO.py
+++ Algorithms/PSO/PSO.py
@@ -1,4 +1,6 @@
 from Algorithms.ALGORITHM import ALGORITHM
+from Problems.SOLUTION import SOLUTION
+import numpy as np
 
 
 class PSO(ALGORITHM):
@@ -16,11 +18,26 @@ class PSO(ALGORITHM):
 
     def Optimization(self):
         Population = self.Problem.Initialization()
-        p_best = Population
-        p_best_fitness = Population.PopObj
-        g_best = 0
-
+        PopDec = Population.decs
+        PopObj = Population.objs
+        N = self.Problem.N
+        M = self.Problem.M
+        p_best = PopDec
+        p_best_fitness = PopObj
+        g_best = p_best[np.argmin(p_best_fitness)]
+        g_best_fitness = p_best_fitness[np.argmin(p_best_fitness)]
+        v = np.zeros((N, M))
         while ALGORITHM().NotTerminated():
-            pass
+            v = v + self.c1 * np.random.random() * (p_best - PopDec) + self.c2 * np.random.random() * (g_best - PopDec)
+            PopDec = PopDec + v
+            Population = SOLUTION(PopDec)
+            PopObj = Population.objs
+            for i in range(N):
+                if PopObj[i] < p_best_fitness[i]:
+                    p_best[i] = Population.decs[i]
+                    p_best_fitness[i] = PopObj[i]
+                if PopObj[i] < g_best_fitness:
+                    g_best = PopDec[i]
+                    g_best_fitness = PopObj[i]
 
         return Population
