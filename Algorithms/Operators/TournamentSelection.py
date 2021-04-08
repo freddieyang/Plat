411ef47f9301c8c37621cedb6a3fdce30a0168a4
freddieyang <yf970113@outlook.com>
2021-04-08 20:50:41 +0800

    完成了交配池选择

---
+++ Algorithms/Operators/TournamentSelection.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def TournamentSelection(K, N, Population):
+    PopObjs = Population.objs
+    rank = np.argsort(np.reshape(PopObjs, len(PopObjs)))
+    Parents = np.random.randint(N, size=(K, np.size(PopObjs, 0)))
+    best = np.argmin(rank[Parents], 0)
+    index = np.diagonal(Parents[best])
+    return index
