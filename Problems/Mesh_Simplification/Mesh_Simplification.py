b3d662c60a91a55fb39edb8ca1776f4387da639c
freddieyang <yf970113@outlook.com>
2021-04-19 18:27:39 +0800

    Add new collapse method

--- Problems/Mesh_Simplification/Mesh_Simplification.py
+++ Problems/Mesh_Simplification/Mesh_Simplification.py
@@ -11,6 +11,48 @@ from Problems.PROBLEM import PROBLEM
 from .Model import Model  # Notice the '.'
 from .file_operation import write_file
 
+np.seterr(divide='ignore', invalid='ignore')
+
+
+def compute_q(v, neighbor_facet, vertex):
+    q = np.zeros((4, 4))
+    for nf in neighbor_facet:
+        vertex_mat = np.array([vertex[nf[0]], vertex[nf[1]], vertex[nf[2]]])
+        abc = np.matmul(np.linalg.inv(vertex_mat), np.array([[1], [1], [1]]))
+        plane = np.concatenate([abc.T, np.array(-1).reshape(1, 1)], axis=1) / (np.sum(abc ** 2) ** 0.5)
+        p = plane.reshape(1, np.size(plane, 1)).reshape((4, 1))
+
+        # ab = vertex[nf[0]] - vertex[nf[1]]
+        # ac = vertex[nf[0]] - vertex[nf[2]]
+        # a, b, c = np.cross(ab, ac) / np.linalg.norm(np.cross(ab, ac))
+        # d = -sum(np.multiply([a, b, c], v))
+        # p = np.array([a, b, c, d]).reshape((4, 1))
+
+        q = q + np.dot(p, np.transpose(p))
+    return q
+
+
+def get_optimal(select_edge, vertex, facet):
+    v_0 = select_edge[0]
+    v_1 = select_edge[1]
+    v_0_neighbor_facet = facet[np.where(v_0 == facet)[0]]
+    v_1_neighbor_facet = facet[np.where(v_1 == facet)[0]]
+    try:
+        v_0_q = compute_q(vertex[v_0], v_0_neighbor_facet, vertex)
+        v_1_q = compute_q(vertex[v_1], v_1_neighbor_facet, vertex)
+        v_optimal_q = v_0_q + v_1_q
+        v_optimal_q = np.concatenate([v_optimal_q[:3, :], np.array([0, 0, 0, 1]).reshape(1, 4)], axis=0)
+        if np.linalg.det(v_optimal_q) > 0:
+            v_optimal = np.matmul(np.linalg.inv(v_optimal_q), np.array([0, 0, 0, 1]).reshape(4, 1))
+            v_optimal = v_optimal.reshape(4)[:3]
+        else:
+            v_optimal = (vertex[v_0] + vertex[v_1]) / 2
+        if np.max(v_optimal) > np.max(vertex) * 1e5 or np.min(v_optimal) < -np.max(vertex) * 1e5:
+            v_optimal = (vertex[v_0] + vertex[v_1]) / 2
+    except np.linalg.LinAlgError:
+        v_optimal = (vertex[v_0] + vertex[v_1]) / 2
+    return v_optimal
+
 
 class Mesh_Simplification(PROBLEM):
     # Initialization of the problem
@@ -25,103 +67,74 @@ class Mesh_Simplification(PROBLEM):
         self.number_of_facet = len(self.facet)
         self.edge = self.Model.edge
         self.number_of_edge = len(self.edge)
-        self.plane_equ_para = []
-        self.Q_matrices = []
-        self.calculate_plane_equations()
-        self.calculate_Q_matrices()
         self.D = len(self.edge)
         self.ratio = parameter
         self.output = output
-
-    def calculate_plane_equations(self):
-        self.plane_equ_para = []
-        for i in range(0, self.number_of_facet):
-            # solving equation ax+by+cz+d=0, a^2+b^2+c^2=1
-            # set d=-1, give three points (x1, y1 ,z1), (x2, y2, z2), (x3, y3, z3)
-            point_1 = self.vertex[self.facet[i, 0] - 1, :]
-            point_2 = self.vertex[self.facet[i, 1] - 1, :]
-            point_3 = self.vertex[self.facet[i, 2] - 1, :]
-            point_mat = np.array([point_1, point_2, point_3])
-            abc = np.matmul(np.linalg.inv(point_mat), np.array([[1], [1], [1]]))
-            self.plane_equ_para.append(
-                np.concatenate([abc.T, np.array(-1).reshape(1, 1)], axis=1) / (np.sum(abc ** 2) ** 0.5))
-        self.plane_equ_para = np.array(self.plane_equ_para)
-        self.plane_equ_para = self.plane_equ_para.reshape(self.plane_equ_para.shape[0], self.plane_equ_para.shape[2])
-
-    def calculate_Q_matrices(self):
-        self.Q_matrices = []
-        for i in range(0, self.number_of_vertex):
-            point_index = i + 1
-            # each point is the solution of the intersection of a set of planes
-            # find the planes for point_index
-            face_set_index = np.where(self.facet == point_index)[0]
-            Q_temp = np.zeros((4, 4))
-            for j in face_set_index:
-                p = self.plane_equ_para[j, :]
-                p = p.reshape(1, len(p))
-                Q_temp = Q_temp + np.matmul(p.T, p)
-            self.Q_matrices.append(Q_temp)
 
     # Calculate the objective of a solution
     def CalObj(self, PopDec):
         PopObj = np.zeros((np.size(PopDec, 0), self.M))
         for i in range(self.N):
+            vertex = copy.copy(self.vertex)
+            facet = copy.copy(self.facet) - 1
+            edge = copy.copy(self.edge) - 1
+            new_vertex_count = 0
+            status_edge = np.ones(self.number_of_edge)
+            j = 0
+            while self.number_of_vertex - new_vertex_count >= self.ratio * self.number_of_vertex:
+                if status_edge[PopDec[i][j]] == 1:
+                    tmp_facet = copy.copy(facet)
+                    select_edge = edge[PopDec[i][j], :]
+                    i_0 = min(select_edge)
+                    i_1 = max(select_edge)
+
+                    vertex[select_edge] = (vertex[i_0] + vertex[i_1]) / 2
+                    # vertex[select_edge] = get_optimal(select_edge, vertex, facet)
+
+                    v_0_in_facet_loc = np.where(i_0 == facet)
+                    v_1_in_facet_loc = np.where(i_1 == facet)
+                    facet_delete_index = np.array(list(set(v_0_in_facet_loc[0]) & set(v_1_in_facet_loc[0])))
+                    facet_update_index = np.where(facet > i_1)
+                    if np.size(v_1_in_facet_loc, 1) > 0:
+                        facet[v_1_in_facet_loc] = i_0
+                    if np.size(facet_update_index, 1) > 0:
+                        facet[facet_update_index] = facet[facet_update_index] - 1
+                    if len(facet_delete_index) > 0:
+                        facet = np.delete(facet, facet_delete_index, 0)
+
+                    v_1_in_edge_loc = np.where(i_1 == edge)
+                    edge_update_index = np.where(edge > i_1)
+                    if np.size(v_1_in_edge_loc, 1) > 0:
+                        edge[v_1_in_edge_loc] = i_0
+                    if np.size(edge_update_index, 1) > 0:
+                        edge[edge_update_index] = edge[edge_update_index] - 1
+
+                    vertex = np.delete(vertex, i_1, 0)
+                    status_edge[np.where(edge[:, 0] - edge[:, 1] == 0)[0]] = 0
+                    status_edge[PopDec[i][j]] = 0
+
+                    j = j + 1
+                    new_vertex_count = new_vertex_count + 1
+                else:
+                    j = j + 1
+            current_path = os.path.dirname(__file__)
+            write_file(vertex, facet, current_path + '\\tmp.obj')
+            p1 = 'models\\' + self.filename
+            p2 = 'tmp.obj'
+            para = 'cd %s && metro.exe %s %s -n' % (current_path, p1, p2)
             try:
-                vertex = copy.copy(self.vertex)
-                facet = copy.copy(self.facet) - 1
-                edge = copy.copy(self.edge) - 1
-                new_vertex_count = 0
-                edge_status = np.ones(self.number_of_edge)
-                j = 0
-                while self.number_of_vertex - new_vertex_count >= self.ratio * self.number_of_vertex:
-                    if edge_status[PopDec[i][j]] == 1:
-                        select_edge = edge[PopDec[i][j], :]
-                        i_0 = select_edge[0]
-                        i_1 = select_edge[1]
-                        v_0 = vertex[i_0, :]
-                        v_1 = vertex[i_1, :]
-                        v_opt = (v_0 + v_1) / 2
-                        vertex[select_edge] = v_opt
-                        v_0_in_facet_loc = np.where(i_0 == facet)
-                        v_1_in_facet_loc = np.where(i_1 == facet)
-                        facet_delete_index = np.array(list(set(v_0_in_facet_loc[0]) & set(v_1_in_facet_loc[0])))
-                        facet_update_index = np.where(facet > i_1)
-                        if np.size(v_1_in_facet_loc, 1) > 0:
-                            for t in range(np.size(v_1_in_facet_loc, 1)):
-                                facet[v_1_in_facet_loc[0][t], v_1_in_facet_loc[1][t]] = i_0
-                        if np.size(facet_update_index, 1) > 0:
-                            for t in range(np.size(facet_update_index, 1)):
-                                facet[facet_update_index[0][t], facet_update_index[1][t]] = \
-                                    facet[facet_update_index[0][t], facet_update_index[1][t]] - 1
-                        v_1_in_edge_loc = np.where(i_1 == edge)
-                        edge_update_index = np.where(edge > i_1)
-                        if np.size(v_1_in_edge_loc, 1) > 0:
-                            for t in range(np.size(v_1_in_edge_loc, 1)):
-                                edge[v_1_in_edge_loc[0][t], v_1_in_edge_loc[1][t]] = i_0
-                        if np.size(edge_update_index, 1) > 0:
-                            for t in range(np.size(edge_update_index, 1)):
-                                edge[edge_update_index[0][t], edge_update_index[1][t]] = \
-                                    edge[edge_update_index[0][t], edge_update_index[1][t]] - 1
-                        if len(facet_delete_index) > 0:
-                            facet = np.delete(facet, facet_delete_index, 0)
-                        vertex = np.delete(vertex, i_1, 0)
-                        edge_status[np.where(edge[:, 0] - edge[:, 1] == 0)[0]] = 0
-                        edge_status[PopDec[i][j]] = 0
-                        j = j + 1
-                        new_vertex_count = new_vertex_count + 1
-                    else:
-                        j = j + 1
-                current_path = os.path.dirname(__file__)
-                write_file(vertex, facet, current_path + '\\tmp.obj')
-                p1 = 'models\\' + self.filename
-                p2 = 'tmp.obj'
-                para = 'cd %s && metro.exe %s %s' % (current_path, p1, p2)
+                # err_1 = np.where(facet[:, 0] - facet[:, 1] == 0)[0]
+                # err_2 = np.where(facet[:, 0] - facet[:, 1] == 0)[0]
+                # err_3 = np.where(facet[:, 0] - facet[:, 1] == 0)[0]
+                #
+                # if len(err_1) > 0 or len(err_2) > 0 or len(err_3) > 0:
+                #     print(123)
                 rc, out = subprocess.getstatusoutput(para)
                 # start_position = out.find('Hausdorff distance:') + 20
                 # end_position = out.find('Hausdorff distance:') + 50
                 # tmp_str = out[start_position:end_position]
                 fitness = float(re.findall(r'Hausdorff distance: (.+?)\(', out)[0])
-            except IndexError:
+            except:
                 fitness = sys.maxsize
             PopObj[i] = fitness
             if fitness < config.best_fitness:
